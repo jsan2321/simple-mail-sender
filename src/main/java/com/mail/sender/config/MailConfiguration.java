ba70aff3fb8a29dee16fa037f4e7c731f0cef833
jsan2321 <jsantiago.c@hotmail.com>
2024-08-28 17:09:50 -0500

    initial commit

---
+++ src/main/java/com/mail/sender/config/MailConfiguration.java
@@ -0,0 +1,30 @@
+package com.mail.sender.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com"); // host name for gmail (email provider)
+        mailSender.setPort(587); //  smtp standard port
+        mailSender.setUsername("alojaxxx@gmail.com");
+        mailSender.setPassword("<Password>");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp"); // setting protocol
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
