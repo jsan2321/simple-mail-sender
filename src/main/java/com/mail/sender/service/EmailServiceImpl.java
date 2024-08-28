557af1d2a5d7c212a0513ba2bc47c49bc47d4af8
jsan2321 <jsantiago.c@hotmail.com>
2024-08-28 18:44:00 -0500

    implemented file sending method

--- src/main/java/com/mail/sender/service/EmailServiceImpl.java
+++ src/main/java/com/mail/sender/service/EmailServiceImpl.java
@@ -1,12 +1,15 @@
 package com.mail.sender.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailServiceImpl implements IEmailService {
@@ -33,6 +36,19 @@ public class EmailServiceImpl implements IEmailService {
 
     @Override
     public void sendEmailWithFile(String[] toUser, String subject, String message, File file) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, StandardCharsets.UTF_8.name());
 
+            mimeMessageHelper.setFrom(emailUser);
+            mimeMessageHelper.setTo(toUser);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(message);
+            mimeMessageHelper.addAttachment(file.getName(), file);
+
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
