3430c6465a54734d18bd0f6997049582e49a6d62
jsan2321 <jsantiago.c@hotmail.com>
2024-08-28 21:26:49 -0500

    added controller

---
+++ src/main/java/com/mail/sender/controller/MailController.java
@@ -0,0 +1,53 @@
+package com.mail.sender.controller;
+
+import com.mail.sender.domain.EmailDTO;
+import com.mail.sender.domain.EmailFileDTO;
+import com.mail.sender.service.IEmailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1")
+public class MailController {
+
+    private final IEmailService emailService;
+    public MailController(IEmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/sendMessage")
+    public ResponseEntity<?> receiveRequestEmail(@RequestBody EmailDTO emailDTO) { // get information from frontend through dto
+        System.out.println("Message received " + emailDTO);
+        emailService.sendEmail(emailDTO.getToUser(), emailDTO.getSubject(), emailDTO.getMessage());
+        Map<String, String> response = new HashMap<>();
+        response.put("state", "Sent");
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/sendMessageFile")
+    public ResponseEntity<?> receiveRequestEmailWithFile(@ModelAttribute EmailFileDTO emailFileDTO) {
+        try {
+            String fileName = emailFileDTO.getFile().getName();
+            Path path = Paths.get("src/main/resources/files/" + fileName); // path where the file is saved
+            Files.createDirectories(path.getParent());
+            Files.copy(emailFileDTO.getFile().getInputStream(), path, StandardCopyOption.REPLACE_EXISTING); // delete duplicate files
+            File file = path.toFile();
+            emailService.sendEmailWithFile(emailFileDTO.getToUser(), emailFileDTO.getSubject(), emailFileDTO.getMessage(), file);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("state", "Sent");
+            response.put("file", "fileName");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            throw new RuntimeException("Error when sending email with file. " + e);
+        }
+    }
+}
--- src/main/java/com/mail/sender/domain/EmailDTO.java
+++ src/main/java/com/mail/sender/domain/EmailDTO.java
@@ -3,10 +3,12 @@ package com.mail.sender.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class EmailDTO { // convert incoming json into object
     private String[] toUser;
     private  String subject;
